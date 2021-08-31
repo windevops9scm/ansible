068c1636b275a7b6a34cf8458a9f132e9f0ee25f
windevops9scm <windevops9@gmail.com>
2021-08-31 17:18:34 +0530

    roboshop- frontend component setup -terraform infra inventory file ode added

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -30,3 +30,8 @@ resource "aws_route53_record" "records" {
 locals {
   LENGTH  = length(var.COMPONENTS)
 }
+
+resource "local_file" "inventory-file" {
+  content     = "[FRONTEND]\n${aws_spot_instance_request.cheap_worker.*.private_ip[9]}\n[PAYMENT]\n${aws_spot_instance_request.cheap_worker.*.private_ip[8]}\n[SHIPPING]\n${aws_spot_instance_request.cheap_worker.*.private_ip[7]}\n[USER]\n${aws_spot_instance_request.cheap_worker.*.private_ip[6]}\n[CATALOGUE]\n${aws_spot_instance_request.cheap_worker.*.private_ip[5]}\n[CART]\n${aws_spot_instance_request.cheap_worker.*.private_ip[4]}\n[REDIS]\n${aws_spot_instance_request.cheap_worker.*.private_ip[3]}\n[RABBITMQ]\n${aws_spot_instance_request.cheap_worker.*.private_ip[2]}\n[MONGODB]\n${aws_spot_instance_request.cheap_worker.*.private_ip[1]}\n[MYSQL]\n${aws_spot_instance_request.cheap_worker.*.private_ip[0]}\n"
+  filename    = "/tmp/inv-roboshop"
+}
