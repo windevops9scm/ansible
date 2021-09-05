56173c0a2e6badf187d556447a8ff5d47c7a3267
windevops9scm <windevops9@gmail.com>
2021-09-05 16:44:37 +0530

    multi environment added

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -15,7 +15,8 @@ resource "aws_ec2_tag" "name-tag" {
   count                 = local.LENGTH
   resource_id           = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
   key                   = "Name"
-  value                 = element(var.COMPONENTS,count.index )
+  value                 = "${element(var.COMPONENTS, count.index)}-${var.ENV}"
+//  element(var.COMPONENTS,count.index )
 }
 
 resource "aws_route53_record" "records" {
