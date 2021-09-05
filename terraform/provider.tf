053ee3580216f7862a4e5b77da9d16c462afff39
windevops9scm <windevops9@gmail.com>
2021-09-05 16:20:04 +0530

    multi environment added

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -3,10 +3,5 @@ provider "aws" {
 }
 
 terraform {
-  backend "s3" {
-    bucket        = "terraform-d34"
-    key           = "roboshop/ansible/terraform.tfstate"
-    region        = "us-east-1"
-    dynamodb_table ="terraform"
-  }
+  backend "s3" {}
 }
